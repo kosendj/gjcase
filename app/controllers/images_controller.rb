6bbeb8a0c476f0296dcf40a881df3ae1a49e836c
Shota Fukumori (sora_h) <her@sorah.jp>
2015-06-15 03:30:10 +0900

    Stop accepting duplication_id
    Duplication control will be provided with proper interface later.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -70,6 +70,6 @@ class ImagesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def image_params
-    params.require(:image).permit(:source_url, :comment, :duplication_id)
+    params.require(:image).permit(:source_url, :comment)
   end
 end
