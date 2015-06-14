15f6876ad7420ccc4f596555fbc93b79c702641a
Shota Fukumori (sora_h) <her@sorah.jp>
2015-06-14 21:49:57 +0900

    indent

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -62,13 +62,14 @@ class ImagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_image
-      @image = Image.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def image_params
-      params.require(:image).permit(:source_url, :comment, :duplication_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_image
+    @image = Image.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def image_params
+    params.require(:image).permit(:source_url, :comment, :duplication_id)
+  end
 end
