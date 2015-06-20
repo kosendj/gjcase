1af7a3d0c2e42a71761411c12f79396513dc80c7
Shota Fukumori (sora_h) <her@sorah.jp>
2015-06-21 07:23:27 +0900

    images#index: tags filter param

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,6 +5,15 @@ class ImagesController < ApplicationController
   # GET /images.json
   def index
     @images = Image.all.page(params[:page])
+
+    if params[:tags]
+      # XXX:
+      tag_queries = [*params[:tags]]
+      tag_ids = Tag.where(id: tag_queries.grep(/\A[0-9]+\z/)).pluck(:id) \
+              + Tag.where(name: tag_queries).pluck(:id)
+      image_ids = TagAssignment.where(tag_id: tag_ids).pluck(:image_id)
+      @images = Image.where(id: image_ids).page(params[:page])
+    end
   end
 
   # GET /images/1
