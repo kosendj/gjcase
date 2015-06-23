eddbe68c6a87f06b23bd6f24fe23fd62785fb9a9
Shota Fukumori (sora_h) <her@sorah.jp>
2015-06-24 02:45:53 +0900

    rescue AR errors

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,12 @@
 class Api::BaseController < ActionController::Base
   include Garage::ControllerHelper
   include Garage::RestfulActions
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render status: 404, json: {"error": {"status": 404, "message": "Not Found"}}
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do
+    render status: 422, json: {"error": {"status": 422, "message": "Not Found"}}
+  end
 end
