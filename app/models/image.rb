39029dc3a2fb10341e5601f1eb035f7b0866a004
Shota Fukumori (sora_h) <her@sorah.jp>
2015-06-26 04:16:39 +0900

    Fix Image#mirror!

--- app/models/image.rb
+++ app/models/image.rb
@@ -48,35 +48,49 @@ class Image < ActiveRecord::Base
 
   def mirror!
     # XXX:
+    Rails.logger.info "Mirroring #{self.inspect}"
     file = Tempfile.new("gjcase-mirror-#{self.id || Digest::SHA2.hexdigest(self.source_url)}")
     system("curl", self.source_url, out: file) or raise 'curl failed'
     file.rewind
+
 
     self.sha = IO.popen([*%w(openssl dgst -sha256), in: file], 'r') do |io|
       out = io.read.chomp
       _, status = Process.waitpid2(io.pid)
       raise 'openssl dgst -sha256 fail' unless status.success?
-      out
+      out.split(' ').last
     end
-    file.rewind
+    Rails.logger.info "Image(#{self.id}) mirror: sha256 = #{self.sha}"
 
     self.set_storage_path!
+    Rails.logger.info "Image(#{self.id}) mirror: storage_path = #{self.storage_path}"
 
     bucket = Rails.application.secrets.storage_master_bucket
     s3 = Aws::S3::Client.new(region: Rails.application.secrets.storage_master_region)
 
+    Rails.logger.info "Image(#{self.id}) mirror: head_object(bucket: #{bucket}, key: #{self.storage_path})"
+
     begin
       head = s3.head_object(bucket: bucket, key: self.storage_path)
-      return if head
+      if head
+        self.save!
+        Rails.logger.info "Image(#{self.id}) mirror: skipping"
+        return
+      end
     rescue Aws::S3::Errors::NotFound
     end
 
+    Rails.logger.info "Image(#{self.id}) mirror: put_object(bucket: #{bucket}, key: #{self.storage_path}, type: #{self.guessed_content_type})"
+
+    file.rewind
     s3.put_object(
       bucket: bucket,
       key: self.storage_path,
       content_type: self.guessed_content_type,
       body: file,
     )
+
+    Rails.logger.info "Image(#{self.id}) mirror: mirrored"
 
     self.save!
   ensure
