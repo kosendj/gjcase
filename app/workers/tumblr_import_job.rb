62639895ca6f0df86b08d8926d4451eea5c37dbe
Shota Fukumori (sora_h) <her@sorah.jp>
2015-06-27 15:58:46 +0900

    they have no offset...

--- app/workers/tumblr_import_job.rb
+++ app/workers/tumblr_import_job.rb
@@ -16,42 +16,27 @@ class TumblrImportJob
     end
 
     def run!
-      no_download_count = 0
-      downloaded = true
-      offset = 0
-      while no_download_count < 10 && offset < @max_offset
-        downloaded = false
-        payload = get_photos(offset)
-        log "=> Offset: #{offset}"
+      payload = get_photos(0)
 
-        break if payload['response'].empty?
+      break if payload['response'].empty?
 
-        link_map = payload['response'].select{ |_| _['type'] == 'photo' && _['photos']}.map do |item|
-          [item['post_url'], comment: item['caption'], images: item['photos'].map { |_| _['original_size']['url'] rescue nil }.compact.select { |_| _.end_with?('.gif') }]
-        end.to_h
+      link_map = payload['response'].select{ |_| _['type'] == 'photo' && _['photos'] }.map do |item|
+        [item['post_url'], comment: item['caption'], images: item['photos'].map { |_| _['original_size']['url'] rescue nil }.compact.select { |_| _.end_with?('.gif') }]
+      end.to_h
 
-        Image.where(source_identifier: link_map.keys).pluck(:source_identifier).each do |x|
-          link_map.delete x
+      Image.where(source_identifier: link_map.keys).pluck(:source_identifier).each do |x|
+        link_map.delete x
+      end
+
+      link_map.each do |link, x|
+        log " * #{link}"
+
+        x[:images].each do |url|
+          comment = x[:comment]
+          image = Image.create!(source_url: url, source_identifier: link, comment: comment, tags: [@tag])
+          ImageMirrorJob.perform_async(image.id)
+          log "   #{url} => #{image.id}"
         end
-
-        link_map.each do |link, x|
-          log " * #{link}"
-
-          x[:images].each do |url|
-            downloaded = true
-            comment = x[:comment]
-            image = Image.create!(source_url: url, source_identifier: link, comment: comment, tags: [@tag])
-            ImageMirrorJob.perform_async(image.id)
-            log "   #{url} => #{image.id}"
-          end
-        end
-        if downloaded
-          no_download_count += 1 
-        else
-          no_download_count = 0
-        end
-        no_download_count = 0
-        offset += 20
       end
     end
 
